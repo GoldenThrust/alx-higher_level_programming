7f77dbd63eb1b2e3441facd7e2e3e961efac9cd3
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-06-06 19:15:48 -0700

    mandatory project on if&else, function and loops

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 
 def uppercase(str):
     for i in str:
-        if ord(i) >= 97 and ord(c) <= 122:
+        if ord(i) >= 97 and ord(i) <= 122:
             i = chr(ord(i) - 32)
         print("{}".format(i), end="")
     print("")
