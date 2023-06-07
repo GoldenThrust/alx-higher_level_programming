a98b1bc7aa4cee67e9f679330743e7ab16335d7b
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-06-06 18:26:00 -0700

    mandatory project on if&else, function and loops

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -3,9 +3,9 @@
 /**
  * insert_node - inserts a number into a sorted singly linked list.
  * @head: pointer to the list head
+ * @number: number to insert
  * Return: the address of the new node, or NULL if it failed
  */
-
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *node = *head;
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(97,123):
+for i in range(97, 123):
     if chr(i) != "q" and chr(i) != "e":
         print(chr(i), end="")
