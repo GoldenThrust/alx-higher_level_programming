1d03173e6d032677917bbf9135af80bdfdd5e2ae
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-06-11 15:14:35 -0700

    mandatory Task

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in i:
+            print("{}".format(j), end=" " if j != i[-1] else "")
+        print()
+
