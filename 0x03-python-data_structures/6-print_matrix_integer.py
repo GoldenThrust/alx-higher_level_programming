933509caa2ff959907a35b03b8987b68fb2cb20d
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-06-11 15:30:49 -0700

    mandatory Task

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{}".format(j), end=" " if j != i[-1] else "")
+            print("{:d}".format(j), end=" " if j != i[-1] else "")
         print()
