514d717ba0c85376f81f9a9a6cc4e135e8e3b0c2
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-06-12 21:27:42 +0000

    Empty

--- 0x03-python-data_structures/7-add_tuple.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-def add_tuple(tuple_a=(), tuple_b=()):
-    tup = tuple_a + (0, 0)
-    tup2 = tuple_b + (0, 0)
-
-    new_tuple = (tup[0] + tup2[0], tup[1] + tup2[1])
-    return new_tuple
