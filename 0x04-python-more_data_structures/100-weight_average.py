5157d5b6aac7bb5e4e765445be1ccdfb96ca7544
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-06-14 17:30:53 -0700

    Advance project

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if my_list and len(my_list):
-        nums, dems = 0
+    if my_list:
+        nums, dems = 0, 0
         for i, j in my_list:
             nums += i * j
             dems += j
