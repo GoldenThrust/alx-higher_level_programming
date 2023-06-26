28fb07c52000c4e4eb3f575d0841b8be575637df
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-06-26 10:11:07 -0700

    Mandatory Tasks

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
+        return True
     except(ValueError, TypeError):
         return False
-    else:
-        return True
