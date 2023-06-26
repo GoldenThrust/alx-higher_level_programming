c13df526b1f097aa39394da5f2d05300892ee480
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-06-26 13:58:56 -0700

    Advance Tasks

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except (TypeError, ValueError):
-        print("Exception {}".format(s.exc_info()[1]), file=s.stderr)
+        print("Exception: {}".format(s.exc_info()[1]), file=s.stderr)
         return False
