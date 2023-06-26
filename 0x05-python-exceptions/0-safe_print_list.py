68b3d110f3223219b7ae1565370d2f796626dde7
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-06-26 10:05:11 -0700

    Mandatory Tasks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     for j in range(x):
         try:
-            print(my_list[m], end="")
+            print(my_list[j], end="")
             i += 1
         except IndexError:
             break
