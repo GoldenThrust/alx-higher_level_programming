ad96a33266533843109ed1b6a9e4a7165ebefc72
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-06-27 16:36:11 -0700

    Mandatory Tasks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,7 +38,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-            self.__size = value
+        self.__size = value
 
     @property
     def position(self):
