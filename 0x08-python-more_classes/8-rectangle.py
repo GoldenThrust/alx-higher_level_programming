4f15789b798d9a00ee9667dd09ecc57b967036d6
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-07-03 18:52:44 -0700

    python classes

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -54,7 +54,7 @@ class Rectangle:
     def bigger_or_equal(rect_1, rect_2):
         """ return rectangle with bigger area """
         if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an imstance of Rectangle")
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
