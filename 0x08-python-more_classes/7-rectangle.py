98d82d74c7ff956ec6a8c540543252e82ca84cb9
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-07-03 18:31:22 -0700

    python classes

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """ rectangle interface """
 
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """ initialized rectangle """
