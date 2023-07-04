f8f7727478312aebd61f8fa5dc04bd3f0017ed20
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-07-03 18:17:24 -0700

    python classes

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -62,6 +62,6 @@ class Rectangle:
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
-    """ print a string when a
-    instance of rectangle has been deleted """
-    print("Bye rectangle...")
+        """ print a string whena a instance
+        of rectangle has been deleted """
+        print("Bye rectangle...")
