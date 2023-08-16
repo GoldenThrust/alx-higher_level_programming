64b2eea016fa8a6cea99f3726adc6ebdfbd113ad
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-08-15 18:29:47 -0700

    SQL

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table of the database hbtn_0c_0
-SELECT `score`, `name` FROM `second_table` WHERE `name` <=> "" ORDER BY `score` DESC
+SELECT `score`, `name` FROM `second_table` WHERE `name` != "" ORDER BY `score` DESC
