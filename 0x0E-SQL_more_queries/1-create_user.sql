af9560afd1e01fc00e06d72edbdff71cdccbac4c
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-08-16 17:11:02 -0700

    SQL join

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,1 +1,5 @@
--- creates the MySQL server user user_0d_1
+-- creates the database hbtn_0d_2 and the user user_0d_2
+CREATE USER
+	IF NOT EXISTS `user_0d_1`@`localhost`
+	       	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO `user_0d_1`@`localhost`;
--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,1 +1,4 @@
 -- creates the table id_not_null
+CREATE TABLE
+	IF NOT EXISTS
+	       	id_not_null (id INT DEFAULT 1, name VARCHAR(256))
