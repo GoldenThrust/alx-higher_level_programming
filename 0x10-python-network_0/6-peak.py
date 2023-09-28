02de00c359d98aa91aa0364c6c9fbff9ace0d8be
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-09-28 15:18:25 -0700

    Peak search

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 "finds a peak in a list of unsorted integers"
 
+
 def find_peak(list_of_integers):
     "finds a peak in a list of list_of_integers"
 
@@ -14,7 +15,7 @@ def find_peak(list_of_integers):
         mid = ((left + right) // 2)
 
         if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1;
+            left = mid + 1
         else:
             right = mid
 
