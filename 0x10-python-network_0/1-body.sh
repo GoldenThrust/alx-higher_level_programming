ff4b1490eee9b774069a61c689d2d5792ff2962c
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-09-28 14:34:32 -0700

    Python Networking

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a GET request to the URL
-# and displays the body of the response
-
+# displays the body of the response
 curl -sL $1
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# akes in a URL and displays all HTTP methods the server will accept
+# displays all HTTP methods the server will accept
 curl -sI $1 | grep 'Allow' | cut -d " " -f 2-
