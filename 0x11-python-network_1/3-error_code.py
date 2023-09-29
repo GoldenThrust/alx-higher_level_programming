74cc617c462f90f361b6ca4e5f66e4fe19af35c9
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-09-29 13:24:07 -0700

    No Commit Message

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+'''
+    takes in a URL, sends a request to the URL
+    and displays the body of the response (decoded in utf-8)
+'''
+
+if __name__ == '__main__':
+    import sys
+    import urllib.request as ur
+    import urllib.error as ue
+
+    url = (sys.argv[1])
+
+    req = ur.Request(url)
+
+    try:
+        with ur.urlopen(req) as res:
+            print(res.read().decode("utf-8"))
+    except ue.HTTPError as err:
+        print('Error code: {}'.format(err.code))
