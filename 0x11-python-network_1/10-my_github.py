77fe2605f2342a62bcd4f89cea886cf574dbdf8e
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-09-29 15:01:11 -0700

    No Commit Message

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+    Takes GitHub credentials (username and password)
+    and uses the GitHub API to display your id
+"""
+
+if __name__ == '__main__':
+    import sys
+    import requests as r
+    from requests.auth import HTTPBasicAuth
+
+    url = 'https://api.github.com/user'
+    authe = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    res = r.get(url, auth=authe)
+
+    rJson = res.json()
+    print(rJson.get("id"))
+
