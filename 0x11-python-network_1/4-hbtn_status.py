c55b4533426ba493886a88eb486ffbe4d0a95059
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-09-29 13:45:21 -0700

    No Commit Message

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+'''
+    Fetches https://alx-intranet.hbtn.io/status
+'''
+
+if __name__ == '__main__':
+    import requests as r
+
+    url = 'https://alx-intranet.hbtn.io/status'
+    urlB = r.get(url)
+
+    print("Body response:")
+    print("\t- type: {}".format(type(urlB.text)))
+    print("\t- content: {}".format(urlB.text))
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+'''
+    takes in a URL, sends a request to the URL and
+    displays the value of the variable X-Request-Id
+    in the response header
+'''
+
+if __name__ == '__main__':
+    import sys
+    import requests as r
+
+    url = sys.argv[1]
+    urlB = r.get(url)
+
+    print(urlB.headers.get('X-Request-Id'))
