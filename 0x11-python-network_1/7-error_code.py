2c09ab9cea29f4a5625ffe1afdff76d711ae99d0
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-09-29 14:03:54 -0700

    No Commit Message

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+'''
+    Takes in a URL, sends a request to the URL
+    and displays the body of the response.
+'''
+
+if __name__ == '__main__':
+    import sys
+    import requests as r
+
+    url = sys.argv[1]
+    urlB = r.get(url)
+
+    if urlB.status_code >= 400:
+        print('Error code: {}'.format(urlB.status_code))
+    else:
+        print(urlB.text)
