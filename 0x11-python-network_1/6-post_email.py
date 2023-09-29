285abbd5776f318cd785efbd3fc4378cc60a68c2
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-09-29 13:59:04 -0700

    No Commit Message

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+'''
+    Takes in a URL and an email address,
+    sends a POST request to the passed URL
+    with the email as a parameter, and finally
+    displays the body of the response
+'''
+
+if __name__ == '__main__':
+    import sys
+    import requests as r
+
+    url = sys.argv[1]
+    emval = {'email': sys.argv[2]}
+    urlB = r.post(url, data=emval)
+
+    print(urlB.text)
