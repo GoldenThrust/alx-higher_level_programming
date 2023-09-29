38c7f6e80657f4852170669f390aa34c0e19e55d
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-09-29 13:01:08 -0700

    No Commit Message

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+'''
+    Take in URL and email param and  send POST 
+    req to the URL with the email as param
+    display the body of the response(decode: utf8)
+'''
+
+if __name__ == '__main__':
+    import sys
+    import urllib.request as ur
+    import urllib.parse as up
+
+    url = (sys.argv[1])
+    eml = {'email': sys.argv[2]}
+
+    data = up.urlencode(eml).encode('ascii')
+    req = ur.Request(url, data)
+
+    with ur.urlopen(req) as res:
+        print(res.read().decode("utf-8"))
