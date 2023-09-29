2fa04d26a5e4d42f029d8a158aeff2223a58955d
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-09-29 10:53:42 -0700

    Project 1 and 2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+''' 
+    Fetches: https://alx-intranet.hbtn.io/status
+    and display the type, content and content in utf8 format.
+'''
+
+if __name__ == '__main__':
+    import urllib.request as ur
+
+    with ur.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        response = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode('utf8')))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+''' 
+    Takes in parameter and display
+    the value of the X-Request-Id variable found
+    in the header of the response.
+'''
+
+if __name__ == '__main__':
+    import sys
+    import urllib.request as ur
+
+    with ur.urlopen(sys.argv[1]) as res:
+        response = res.header
+        print(dict(response).get('X-Request-Id'))
+
