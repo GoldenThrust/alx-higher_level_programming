f0ba6d9969d0e75b54a7f7e16df196d1e3821d17
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-09-29 15:15:02 -0700

    No Commit Message

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+    Takes GitHub credentials (username and password)
+    and uses the GitHub API to display your id
+"""
+
+if __name__ == '__main__':
+    import sys
+    import requests as r
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+                    sys.argv[2], sys.argv[1])
+
+    res = r.get(url)
+
+    coms = res.json()
+
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                  coms[i].get("sha"),
+                  coms[i].get("commit")
+                  .get("author").get("name")))
+    except IndexError:
+        pass
