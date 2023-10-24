35f96eba6635a3fa5061fc2bcd3c3875f2bbd075
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-10-24 08:14:27 -0700

    Testing

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 const file = process.argv[2];
 
-fs.readFile(file, 'utf8', function(error, data) {
-	if (error) {
-		console.log(error)
-	} else {
-		console.log(data)
-	}
-})
+fs.readFile(file, 'utf8', function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
